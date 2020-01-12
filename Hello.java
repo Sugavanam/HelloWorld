549198593604756957af445594fa2ef99e615bb5
venkat <venkatsoft98@gmail.com>
2020-01-12 09:45:58 +0530

    Fibonacci series

--- Hello.java
+++ Hello.java
@@ -1,12 +1,18 @@
 
 public class Hello {
 
+	static int fib(int n)
+	{
+		if(n<=1)
+			return n;
+		else
+			return fib(n-1)+fib(n-2);
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-for(int i=0;i<=10;i++) {
+    int n=9;
+    System.out.println(fib(n));}
 	
-System.out.println("Hello"+i);
-	}
-
-}
 }
